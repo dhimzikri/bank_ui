6ac11c8552b71c69e568b7c9dc2e897de32da25e
dhimzikri <dschok182@gmail.com>
2022-08-23 10:03:44 +0700

    changed_md:gridcol2

--- src/components/Paket.jsx
+++ src/components/Paket.jsx
@@ -5,23 +5,32 @@ import { listPaket } from "../constant";
 const Paket = () => {
   return (
     <section id="section">
-      <h2 className={`${styles.heading2} flex items-center justify-center h-auto w-auto -mt-10`}>Daftar Paket</h2>
-      <div className="grid grid-cols-4 gap-9">
-        {listPaket && listPaket.map((n) => {
-          return (
-            <div key={n.id} className="bg-emerald-600 rounded-xl p-4 mt-24">
-              <div className="flex items-center justify-center">
-                <img
-                  src={n.img}
-                  alt=""
-                  className="rounded-xl h-[150px] w-[300px] -mt-24"
-                />
+      <h2
+        className={`${styles.heading2} flex items-center justify-center h-auto w-auto -mt-10`}
+      >
+        Daftar Paket
+      </h2>
+      <div className="grid grid-cols-4 gap-9 md:grid-cols-2">
+        {listPaket &&
+          listPaket.map((n) => {
+            return (
+              <div key={n.id} className="bg-emerald-600 rounded-xl p-4 mt-24 ">
+                <div className="flex items-center justify-center">
+                  <img
+                    src={n.img}
+                    alt=""
+                    className="rounded-xl h-[150px] w-[300px] -mt-24 md:-mt-12 md:h-fit md:w-[]"
+                  />
+                </div>
+                <p className="p-2 text-white font-poppins font-semibold flex items-center justify-center ">
+                  {n.name}
+                </p>
+                <p className="p-2 text-dimWhite font-poppins font-semibold flex items-start justify-start">
+                  {n.desc}
+                </p>
               </div>
-              <p className="p-2 text-white font-poppins font-semibold flex items-center justify-center">{n.name}</p>
-              <p className="p-2 text-dimWhite font-poppins font-semibold flex items-start justify-start">{n.desc}</p>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </section>
   );
